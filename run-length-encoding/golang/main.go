8c49a3ba157a2a058ee06ea36c89036ddae31bf9
Borja Luengo <borja.luengoprieto@affirm.com>
2024-09-25 11:41:00 +0200

    Update placeholder for Go

--- run-length-encoding/golang/main.go
+++ run-length-encoding/golang/main.go
@@ -1,7 +1,31 @@
+/*   Problem statement:  */
+//
+//     Implement compress() and decompress() functions
+//   for a basic string compression/decompression scheme.
+//   In particular, we would like to compress strings with
+//   long runs of the same character.
+//
+//   For example:
+//   - compress "aaabbbbbbccddddaa" into "a3b6c2d4a2".
+//   - decompress "a3b6c2d4a2" into "aaabbbbbbccddddaa".
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	input := inputs()
+	compressed := compress(input)
+	fmt.Println(compressed)
+	// decompressed := decompress(input)
+	// fmt.Println(decompressed)
+}
+
+func compress(input string) string {
+	result := ""
+	return result + input
+}
+
+func decompress(input string) string {
+	result := ""
+	return result + input
 }
